c096e2528fc6ac6540a2cf437e057bc2f90d686b
Prashant <prashantudale@gmail.com>
2025-05-13 18:29:28 +0530

    profile page id updated

--- src/main/java/POM/profilePage.java
+++ src/main/java/POM/profilePage.java
@@ -1,20 +1,35 @@
 package POM;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class profilePage {
+	private WebDriver driver;
 	
-	@FindBy(xpath = ("//h1[contains(text(), 'Log out')]"))
+	@FindBy(xpath = ("//div[@id ='logoutMain']"))
 	private WebElement logOutProfilebtn;
 	
 	@FindBy(xpath = ("//div[contains(@class, 'w-full') and contains(@class, 'h-full') and contains(@class, 'rounded-full') and contains(@class, 'flex') and contains(@class, 'justify-center') and contains(@class, 'items-center') and contains(@class, 'absolute') and contains(@class, 'inset-0')]"))
 	private WebElement logoutDialogBox;
 	
-	@FindBy(xpath = (""))
+	@FindBy(xpath = ("//div[@id ='logoutbtn']"))
 	private WebElement logoutDialogBoxbtn;
+	
+	
 	
 	@FindBy(xpath = (""))
 	private WebElement logoutDialogBoxCancel;
+	
+	
+	public void clickOnLogoutBtn() {
+		logOutProfilebtn.click();
+	}
 
+	// Constructor
+    public profilePage(WebDriver lastDriver) {
+        this.driver = lastDriver;
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/test/java/Test/profilePageTest.java
+++ src/test/java/Test/profilePageTest.java
@@ -1,5 +1,17 @@
 package Test;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import POM.profilePage;
+
 public class profilePageTest {
 
+	@Test(description = " Verify profile page loads", dependsOnMethods = "Test.homePageTest.goToProfilePage")
+	public void verifyProfilePageLoad(ITestContext context) {
+		WebDriver driver = (WebDriver) context.getAttribute("validWebDriver");
+		profilePage profile = new profilePage(driver); 
+		profile.clickOnLogoutBtn();
+	}
 }
