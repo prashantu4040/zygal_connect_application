a243a8c06afbc184638c91be96e55167e01d511a
Prashant <prashantudale@gmail.com>
2025-05-14 13:15:25 +0530

    Login & Logout flow completed

--- src/test/java/Test/profilePageTest.java
+++ src/test/java/Test/profilePageTest.java
@@ -1,6 +1,7 @@
 package Test;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,19 @@ public class profilePageTest {
 	@Test(description = " Verify profile page loads", dependsOnMethods = "Test.homePageTest.goToProfilePage")
 	public void verifyProfilePageLoad(ITestContext context) {
 		WebDriver driver = (WebDriver) context.getAttribute("validWebDriver");
+		profilePage profile = new profilePage(driver);
+		Assert.assertTrue(profile.isDataHolderVisible(), "DataHolder section is not visible");
+		Assert.assertTrue(profile.isFooterVisible(), "Footer is not visible");
+		Assert.assertTrue(profile.isLogoutSectionVisible(), "Logout button inside footer is not visible");
+		profile.clickOnLogoutProfileBtn();
+		
+	}
+	
+	@Test(description = "Logged out successfully", dependsOnMethods = "verifyProfilePageLoad")
+	public void verifySuccessfulLogout(ITestContext context) {
+		WebDriver driver = (WebDriver) context.getAttribute("validWebDriver");
 		profilePage profile = new profilePage(driver); 
-		profile.clickOnLogoutBtn();
+		profile.clickOnLogoutbtn();
+		Assert.assertTrue(profile.isLogoutToastVisible(), "Logout toast message not visible.");
 	}
 }
