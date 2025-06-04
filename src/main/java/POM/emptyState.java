cbeaedff076480c4816e9473e94661519a55c1c7
Prashant <prashantudale@gmail.com>
2025-06-04 17:46:30 +0530

    updated pom class in empty state pom class

--- src/main/java/POM/emptyState.java
+++ src/main/java/POM/emptyState.java
@@ -1,5 +1,48 @@
 package POM;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class emptyState {
+	private WebDriver driver;
 	
+	@FindBy()
+	private WebElement noDataCamImage;
+	
+	@FindBy()
+	private WebElement noDataSpacesImage;
+	
+	@FindBy()
+	private WebElement countOfCamera;
+	
+	@FindBy()
+	private WebElement countOfSpaces;
+	
+	@FindBy()
+	private WebElement camSearch;
+	
+	@FindBy()
+	private WebElement spacesSearch;
+	
+	@FindBy()
+	private WebElement aiSolution;
+	
+	@FindBy()
+	private WebElement collection;
+	
+	@FindBy()
+	private WebElement systemStatus;
+	
+	@FindBy()
+	private WebElement cameraStatus;
+	
+	@FindBy()
+	private WebElement spaceStatus;
+	
+	@FindBy()
+	private WebElement backButton;
+	
+	@FindBy()
+	private WebElement goToHomeButton;
 }
